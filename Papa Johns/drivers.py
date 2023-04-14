3b83a86ee0f6d8981e0700fcc399aa4bd2fe50fb
mason09092261 <98056482+mason09092261@users.noreply.github.com>
2023-04-13 22:27:27 -0400

    Update drivers.py
    added some comments

--- Papa Johns/drivers.py
+++ Papa Johns/drivers.py
@@ -17,7 +17,7 @@ def cleanData(raw, clean):
 
     dataset=dataset.drop(drops)
     dataset.to_csv(clean)
-
+#get data and split into 30 minutes time periods
 orders = pd.DataFrame()
 dataset = pd.read_csv('UK_data_2.csv')
 dataset['Out_the_door_timestamp'] = pd.to_datetime(dataset['Out_the_door_timestamp'])
@@ -33,7 +33,7 @@ orders = orders.sort_values(by=['Date','Time'])
 #print(orders)
 #orders.to_csv('UK_orders_30Min.csv')
 
-
+#convert to numbers
 test = orders.apply(lambda r : pd.datetime.combine(r['Date'],r['Time']),1)
 orders["date_delta"] = (pd.to_numeric((test.dt.strftime("%Y%m%d%H%M%S"))))
 print(orders["date_delta"])
@@ -43,7 +43,7 @@ y = np.array(orders["Count"])
 xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2)
 
 # Use pip install lightgbm to install it on your system
-
+#put into model
 model = ltb.LGBMRegressor()
 model.fit(xtrain, ytrain)
 ypred = model.predict(xtest)
@@ -52,7 +52,7 @@ data2 = pd.DataFrame(data={"Actual Orders": ytest.flatten()})
 print(data.head())
 print(data2.head())
 
-
+#print accuracy report
 from sklearn.metrics import confusion_matrix ,classification_report 
 threshold_nor=2
 y_pred_nor=[0 if c<=threshold_nor else 1 for c in ypred]
